9cea078ee3b49f485ee9e9731225bc8b7106cc5c
Julian Cubillos <julian.cubillos@jikkosoft.com>
2024-08-06 17:00:12 -0500

    basicos de react native y configuracion inicial

--- App.js
+++ App.js
@@ -1,11 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+} from "react-native";
+
+// import icon from './assets/icon.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ color: "white" }}>Abrimos las aplicación</Text>
+      <Image
+        source={{
+          uri: "https://img.asmedia.epimg.net/resizer/v2/W5XTLTDBR5HBLGGJ6HAU7NRDNU.jpg?auth=9a5c61ba0c57bda64c5b30ab527153b563cb919a3c3d8a8fc57f9564278e1b4d&width=360",
+        }}
+        style={{ width: 360, height: 450, resizeMode: "contain" }}
+      />
+      <Button title="Pulsa aquí" onPress={() => alert("Hola")} />
+      <TouchableHighlight
+        underlayColor={"#09f"}
+        onPress={() => alert("Chao")}
+        style={{
+          width: 200,
+          height: 200,
+          backgroundColor: "red",
+          borderRadius: 100,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text style={{ color: "white" }}>Este es otro boton</Text>
+      </TouchableHighlight>
+      <Pressable
+        onPress={() => {
+          alert("Que mas???");
+        }}
+        style={({ pressed }) => [
+          {
+            backgroundColor: pressed ? "rgb(210,230,255)" : "white",
+          },
+          styles.wrapperCustom,
+        ]}
+      >
+        {({ pressed }) => (
+          <Text style={{ fontSize: pressed ? 32 : 16 }}>
+            {pressed ? "Este tambien" : "O no?!!!"}
+          </Text>
+        )}
+      </Pressable>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +61,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
